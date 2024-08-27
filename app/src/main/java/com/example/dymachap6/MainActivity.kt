d694461a3d58e8e4221b41f6275f27e728cbeaee
Ramzy Kermad <kermad.r@sncf.fr>
2024-08-27 15:35:43 +0200

    Updated code for lesson 6 - Drawer menus

--- app/src/main/java/com/example/dymachap6/MainActivity.kt
+++ app/src/main/java/com/example/dymachap6/MainActivity.kt
@@ -4,16 +4,23 @@ import android.accessibilityservice.GestureDescription.StrokeDescription
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity(), FragmentTransactionHandler {
 
     lateinit var textView1: TextView
+
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +36,46 @@ class MainActivity : AppCompatActivity(), FragmentTransactionHandler {
         this.addFragment1ToActivity()
         this.displayAlertDialog()
         this.displayToast()
+
+        this.setUpDrawerMenu()
+    }
+
+    private fun setUpDrawerMenu() {
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val navigationView = findViewById<NavigationView>(R.id.navigation_view)
+
+        this.toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer_text, R.string.close_drawer_string)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navigationView.setNavigationItemSelectedListener {
+
+            when(it.itemId) {
+                R.id.accueil -> Toast.makeText(this, "Accueil", Toast.LENGTH_LONG).show()
+                R.id.profile_item -> Toast.makeText(this, "Profil", Toast.LENGTH_LONG).show()
+                R.id.messages_item -> Toast.makeText(this, "Messages", Toast.LENGTH_LONG).show()
+                R.id.rate_app_item -> Toast.makeText(this, "Note", Toast.LENGTH_LONG).show()
+                R.id.share_app_item -> Toast.makeText(this, "Partager", Toast.LENGTH_LONG).show()
+                R.id.activity_2_item -> {
+                    Intent(this, SecondActivity::class.java).also {
+                        drawerLayout.closeDrawer(GravityCompat.START)
+                        startActivity(it)
+                    }
+                    Toast.makeText(this, "Activité 3", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(toggle.onOptionsItemSelected(item)) return true
+
+        return super.onOptionsItemSelected(item)
     }
 
     private fun displayAlertDialog() {
